514be9e3f57d686f59593c774b3d17263cdfefd5
mahendra-ring <47548350+mahendra-ring@users.noreply.github.com>
2019-03-29 14:48:42 +0530

    Create content.pp

---
+++ manifests/content.pp
@@ -0,0 +1,31 @@
+# A description of what this class does
+#
+# @summary A short summary of the purpose of this class
+#
+# @example
+class mywebserver::content {
+  $vfqdn = $facts['fqdn']
+  Notify{'Writing eBiz content files..':}
+  file{'/var/www/html/ebiz':
+    ensure => 'directory',
+    owner => 'apache',
+    group => 'apache',
+    mode => '0774',
+  }
+  file{'servid.html':
+    content => "This page is hosted in $vfqdn",
+    path=>'/var/www/html/ebiz/servid.html',
+    owner => 'apache',
+    group => 'apache',
+    mode => '0744',
+  }
+  file{'index.html':
+   # content => "Welcome to Default E-Biz hosting page: please contact E-BIZ infrastructire group for next steps..",
+    source => 'puppet:///modules/ebizapache/index.html',
+    path=>'/var/www/html/ebiz/index.html',
+    owner => 'apache',
+    group => 'apache',
+    mode => '0744',
+  }
+  
+}
