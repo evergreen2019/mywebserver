2d4c708847d605217b5f078c3598cc6d7f4d87fe
mahendra-ring <47548350+mahendra-ring@users.noreply.github.com>
2019-03-29 14:49:15 +0530

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,5 +8,5 @@ class mywebserver {
 notify {"Initializing the mywebserver apache server automated configuration...":}
   class { 'mywebserver::install': }
   class { 'mywebserver::vhosts': }
-  class {'mywebserver::content':}
+  class { 'mywebserver::content': }
 }
