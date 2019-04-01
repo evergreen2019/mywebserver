66638bef9ab16d5e2db5e301580592fb9b014681
mahendra-ring <47548350+mahendra-ring@users.noreply.github.com>
2019-04-01 11:34:29 +0530

    Update content.pp

--- manifests/content.pp
+++ manifests/content.pp
@@ -5,16 +5,16 @@
 # @example
 class mywebserver::content {
   $vfqdn = $facts['fqdn']
-  Notify{'Writing eBiz content files..':}
-  file{'/var/www/html/ebiz':
+  Notify{'Writing mahendra content files..':}
+  file{'/var/www/html/mahendra':
     ensure => 'directory',
     owner => 'www-data',
     group => 'www-data',
     mode => '0774',
   }
   file{'servid.html':
-    content => "This page is hosted in $vfqdn",
-    path=>'/var/www/html/ebiz/servid.html',
+    content => "This page is hosted in mahendra`s server: $vfqdn",
+    path=>'/var/www/html/mahendra/servid.html',
     owner => 'www-data',
     group => 'www-data',
     mode => '0744',
